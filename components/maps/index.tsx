cb03cd9c3218f3da67c1619743ce9b25a13178dc
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-08-27 15:50:33 -0600

    propertires showing but not expanding

--- components/maps/index.tsx
+++ components/maps/index.tsx
@@ -1,16 +1,19 @@
 import React, { useState, useRef } from 'react';
+import { PropertyResults } from '../../types/maps';
 import {
   GoogleMap,
   LoadScript,
   Marker,
   DrawingManager
 } from '@react-google-maps/api';
+import PropertyListView from './properties/propertyList';
 
 interface MapComponentProps {
   apiKey: string;
   center: { lat: number; lng: number };
   markers: { lat: number; lng: number }[];
   zoom: number;
+  propertyResults: PropertyResults[]; // Ensure you pass this prop correctly
 }
 
 const mapContainerStyle = {
@@ -22,12 +25,15 @@ const MapComponent: React.FC<MapComponentProps> = ({
   apiKey,
   center,
   markers,
-  zoom
+  zoom,
+  propertyResults // Destructure propertyResults from props
 }) => {
   const [drawingMode, setDrawingMode] =
     useState<google.maps.drawing.OverlayType | null>(null);
   const [shapeDrawn, setShapeDrawn] = useState(false);
   const [boundaryApplied, setBoundaryApplied] = useState(false);
+  const [isPropertyListVisible, setIsPropertyListVisible] = useState(true);
+
   const shapeRef = useRef<any>(null);
 
   const onShapeComplete = (shape: any) => {
@@ -56,6 +62,14 @@ const MapComponent: React.FC<MapComponentProps> = ({
     }
   };
 
+  const handlePropertyListOpen = () => {
+    console.log('Property list opened');
+  };
+
+  const handlePropertyListClose = () => {
+    console.log('Property list closed');
+    setIsPropertyListVisible(false);
+  };
   const handleRemoveBoundaries = () => {
     if (shapeRef.current) {
       shapeRef.current.setMap(null); // Remove the shape completely
@@ -67,110 +81,121 @@ const MapComponent: React.FC<MapComponentProps> = ({
 
   return (
     <LoadScript googleMapsApiKey={apiKey} libraries={['drawing']}>
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        center={center}
-        zoom={zoom}
-      >
-        {markers.map((marker, index) => (
-          <Marker key={index} position={marker} />
-        ))}
+      <div className="relative h-full w-full">
+        <GoogleMap
+          mapContainerStyle={mapContainerStyle}
+          center={center}
+          zoom={zoom}
+        >
+          {markers.map((marker, index) => (
+            <Marker key={index} position={marker} />
+          ))}
 
-        {!boundaryApplied && (
-          <div className="absolute left-1/2 top-4 z-10 -translate-x-1/2 transform rounded-lg bg-white p-2 text-center opacity-50 shadow-lg transition-opacity duration-300 hover:opacity-100">
-            {!drawingMode ? (
-              <p className="mb-2 text-sm font-semibold text-gray-800">
-                Draw a shape around the properties you`d like to search for
-              </p>
-            ) : (
-              <p className="mb-2 text-center text-sm font-semibold text-gray-800">
-                Start Drawing!
-              </p>
-            )}
-            {!shapeDrawn && (
-              <div className="mb-2 flex justify-center space-x-2">
-                <button
-                  className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
-                  onClick={() =>
-                    setDrawingMode(google.maps.drawing.OverlayType.POLYGON)
-                  }
-                >
-                  Polygon
-                </button>
-                <button
-                  className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
-                  onClick={() =>
-                    setDrawingMode(google.maps.drawing.OverlayType.RECTANGLE)
-                  }
-                >
-                  Rectangle
-                </button>
-                <button
-                  className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
-                  onClick={() =>
-                    setDrawingMode(google.maps.drawing.OverlayType.CIRCLE)
-                  }
-                >
-                  Circle
-                </button>
-                <button
-                  className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
-                  onClick={() =>
-                    setDrawingMode(google.maps.drawing.OverlayType.POLYLINE)
-                  }
-                >
-                  Polyline
-                </button>
-              </div>
-            )}
-            {shapeDrawn && (
-              <div className="flex justify-center space-x-4">
-                <button
-                  onClick={handleCancel}
-                  className="rounded bg-gray-300 px-4 py-1 text-black shadow hover:bg-gray-400"
-                >
-                  Cancel
-                </button>
-                <button
-                  onClick={handleApply}
-                  className="rounded bg-blue-600 px-4 py-1 text-white shadow hover:bg-blue-700"
-                >
-                  Apply
-                </button>
-              </div>
-            )}
-          </div>
+          {!boundaryApplied && (
+            <div className="absolute left-1/2 top-4 z-10 -translate-x-1/2 transform rounded-lg bg-white p-2 text-center opacity-50 shadow-lg transition-opacity duration-300 hover:opacity-100">
+              {!drawingMode ? (
+                <p className="mb-2 text-sm font-semibold text-gray-800">
+                  Draw a shape around the properties you`d like to search for
+                </p>
+              ) : (
+                <p className="mb-2 text-center text-sm font-semibold text-gray-800">
+                  Start Drawing!
+                </p>
+              )}
+              {!shapeDrawn && (
+                <div className="mb-2 flex justify-center space-x-2">
+                  <button
+                    className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
+                    onClick={() =>
+                      setDrawingMode(google.maps.drawing.OverlayType.POLYGON)
+                    }
+                  >
+                    Polygon
+                  </button>
+                  <button
+                    className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
+                    onClick={() =>
+                      setDrawingMode(google.maps.drawing.OverlayType.RECTANGLE)
+                    }
+                  >
+                    Rectangle
+                  </button>
+                  <button
+                    className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
+                    onClick={() =>
+                      setDrawingMode(google.maps.drawing.OverlayType.CIRCLE)
+                    }
+                  >
+                    Circle
+                  </button>
+                  <button
+                    className="rounded bg-blue-600 px-2 py-1 text-xs text-white hover:bg-blue-700"
+                    onClick={() =>
+                      setDrawingMode(google.maps.drawing.OverlayType.POLYLINE)
+                    }
+                  >
+                    Polyline
+                  </button>
+                </div>
+              )}
+              {shapeDrawn && (
+                <div className="flex justify-center space-x-4">
+                  <button
+                    onClick={handleCancel}
+                    className="rounded bg-gray-300 px-4 py-1 text-black shadow hover:bg-gray-400"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={handleApply}
+                    className="rounded bg-blue-600 px-4 py-1 text-white shadow hover:bg-blue-700"
+                  >
+                    Apply
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+
+          {boundaryApplied && (
+            <div
+              onClick={handleRemoveBoundaries}
+              className="absolute right-4 top-4 z-10 flex cursor-pointer items-center rounded-lg bg-red-500 px-4 py-2 text-white shadow-lg hover:bg-red-600"
+            >
+              <span className="mr-2">Remove Boundaries</span>
+              <button>&#x2715;</button>
+            </div>
+          )}
+
+          <DrawingManager
+            onPolygonComplete={onShapeComplete}
+            onRectangleComplete={onShapeComplete}
+            onCircleComplete={onShapeComplete}
+            onPolylineComplete={onShapeComplete}
+            options={{
+              drawingControl: false,
+              drawingMode: drawingMode,
+              polygonOptions: {
+                fillColor: '#2196F3',
+                fillOpacity: 0.5,
+                strokeWeight: 2,
+                clickable: false,
+                editable: true,
+                zIndex: 1
+              }
+            }}
+          />
+        </GoogleMap>
+
+        {/* Conditionally render PropertyListView if more than one property */}
+        {isPropertyListVisible && propertyResults.length > 1 && (
+          <PropertyListView
+            properties={propertyResults}
+            onOpen={handlePropertyListOpen}
+            onClose={handlePropertyListClose}
+          />
         )}
-
-        {boundaryApplied && (
-          <div
-            onClick={handleRemoveBoundaries}
-            className="absolute right-4 top-4 z-10 flex cursor-pointer items-center rounded-lg bg-red-500 px-4 py-2 text-white shadow-lg hover:bg-red-600"
-          >
-            <span className="mr-2">Remove Boundaries</span>
-            <button>&#x2715;</button>
-          </div>
-        )}
-
-        <DrawingManager
-          onPolygonComplete={onShapeComplete}
-          onRectangleComplete={onShapeComplete}
-          onCircleComplete={onShapeComplete}
-          onPolylineComplete={onShapeComplete}
-          options={{
-            drawingControl: false,
-            drawingMode: drawingMode,
-            polygonOptions: {
-              fillColor: '#2196F3',
-              fillOpacity: 0.5,
-              strokeWeight: 2,
-              clickable: false,
-              editable: true,
-              zIndex: 1
-            }
-          }}
-        />
-      </GoogleMap>
+      </div>
     </LoadScript>
   );
 };
---
+++ constants/data/properties.ts
@@ -0,0 +1,40 @@
+import { PropertyResults } from '@/types/maps';
+
+export const fetchedPropertiesMockData: PropertyResults[] = [
+  {
+    street: '1752 S Kline Way Apt H',
+    city: 'Lakewood',
+    state: 'CO',
+    zip_code: '80232',
+    beds: 1,
+    full_baths: 1,
+    sqft: 1470,
+    list_price: '$277,735.00',
+    last_sold_date: '12/29/2017',
+    sold_price: '$170,500.00'
+  },
+  {
+    street: '37 S Eagle Cir',
+    city: 'Aurora',
+    state: 'CO',
+    zip_code: '80012',
+    beds: 2,
+    full_baths: 2,
+    sqft: 1790,
+    list_price: '$489,670.00',
+    last_sold_date: '11/04/2004',
+    sold_price: '$177,000.00'
+  },
+  {
+    street: '7420 W Mexico Dr',
+    city: 'Lakewood',
+    state: 'CO',
+    zip_code: '80232',
+    beds: 3,
+    full_baths: 2,
+    sqft: 1600,
+    list_price: '$450,000.00',
+    last_sold_date: '06/15/2018',
+    sold_price: '$185,000.00'
+  }
+];
