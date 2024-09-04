1e45e88d67eb3fdb807eb28df0bcd3936b0ec3b4
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-09-04 17:19:22 -0600

    propertydetails dark mode fixed

--- components/property/page/landLocationInformation.tsx
+++ components/property/page/landLocationInformation.tsx
@@ -26,15 +26,19 @@ const LandLocationInformationComponent: React.FC<LandLocationProps> = ({
   const renderLandLocationDetails = (label: string, value: any) => {
     return (
       <div className="mb-4 flex flex-col items-start">
-        <span className="font-semibold text-gray-500">{label}</span>
+        <span className="font-semibold text-gray-500 dark:text-gray-400">
+          {label}
+        </span>
         <span>{value ?? '-'}</span>
       </div>
     );
   };
 
   return (
-    <div className="rounded-lg bg-white p-6 shadow-md">
-      <h2 className="mb-4 text-xl font-bold">Land/Location Information</h2>
+    <div className="rounded-lg bg-white p-6 shadow-md dark:bg-gray-800 dark:shadow-lg">
+      <h2 className="mb-4 text-xl font-bold dark:text-white">
+        Land/Location Information
+      </h2>
       <div className="grid grid-cols-2 gap-6 sm:grid-cols-3 lg:grid-cols-4">
         {renderLandLocationDetails('Lot Size', landLocation.lot_size)}
         {renderLandLocationDetails('Lot Area', landLocation.lot_area)}
@@ -43,7 +47,7 @@ const LandLocationInformationComponent: React.FC<LandLocationProps> = ({
           landLocation.property_class
         )}
         {/* {renderLandLocationDetails('APN', landLocation.apn)}
-                {renderLandLocationDetails('Zoning', landLocation.zoning)} */}
+            {renderLandLocationDetails('Zoning', landLocation.zoning)} */}
         {renderLandLocationDetails('Census Tract', landLocation.census_tract)}
         {renderLandLocationDetails('Block', landLocation.block)}
         {renderLandLocationDetails('Lot Number', landLocation.lot_number)}
@@ -56,8 +60,8 @@ const LandLocationInformationComponent: React.FC<LandLocationProps> = ({
           landLocation.neighborhood_type
         )}
         {/* <div className="col-span-2">
-                    {renderLandLocationDetails('Legal Description', landLocation.legal_description)}
-                </div> */}
+            {renderLandLocationDetails('Legal Description', landLocation.legal_description)}
+        </div> */}
       </div>
     </div>
   );
