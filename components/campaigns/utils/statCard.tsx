243e8bb33ff1c354a00ba50f6828119cc47f8a14
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-09-06 00:16:11 -0600

    added campaigns animation and click

--- components/campaigns/utils/statCard.tsx
+++ components/campaigns/utils/statCard.tsx
@@ -3,14 +3,30 @@ import React from 'react';
 interface StatCardProps {
   title: string;
   value: number;
-  onClick: () => void; // New onClick prop for handling the click action
+  onClick: () => void;
+  isActive: boolean; // Prop to check if the card is active
+  click: boolean; // Prop to check if the card can be clicked
+  animationComplete: boolean; // Prop to check if the animation should stop
 }
 
-const StatCard: React.FC<StatCardProps> = ({ title, value, onClick }) => {
+const StatCard: React.FC<StatCardProps> = ({
+  title,
+  value,
+  onClick,
+  isActive,
+  click,
+  animationComplete
+}) => {
   return (
     <div
-      onClick={onClick}
-      className="cursor-pointer rounded-md bg-white p-4 text-center shadow-md transition-all hover:outline hover:outline-2 hover:outline-gray-300 dark:bg-gray-800 dark:text-white dark:hover:outline-gray-600"
+      onClick={click ? onClick : undefined} // Only trigger onClick if 'click' is true
+      className={`${
+        click && `cursor-pointer`
+      } rounded-md bg-white p-4 text-center shadow-md transition-all dark:bg-gray-800 dark:text-white ${
+        click && isActive && !animationComplete && value > 0
+          ? 'animate-pulse border-4 border-orange-500 dark:border-gray-400' // Animate if not complete
+          : ''
+      }`}
     >
       <p>{title}</p>
       <h2 className="text-3xl font-bold">{value}</h2>
