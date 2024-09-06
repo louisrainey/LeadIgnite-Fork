222938072f79194f431d38855fd925b63eeeb4c4
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-09-05 23:52:47 -0600

    added campaign header

--- components/campaigns/utils/statCard.tsx
+++ components/campaigns/utils/statCard.tsx
@@ -3,11 +3,15 @@ import React from 'react';
 interface StatCardProps {
   title: string;
   value: number;
+  onClick: () => void; // New onClick prop for handling the click action
 }
 
-const StatCard: React.FC<StatCardProps> = ({ title, value }) => {
+const StatCard: React.FC<StatCardProps> = ({ title, value, onClick }) => {
   return (
-    <div className="rounded-md bg-white p-4 text-center shadow-md dark:bg-gray-800 dark:text-white">
+    <div
+      onClick={onClick}
+      className="cursor-pointer rounded-md bg-white p-4 text-center shadow-md transition-all hover:outline hover:outline-2 hover:outline-gray-300 dark:bg-gray-800 dark:text-white dark:hover:outline-gray-600"
+    >
       <p>{title}</p>
       <h2 className="text-3xl font-bold">{value}</h2>
     </div>
