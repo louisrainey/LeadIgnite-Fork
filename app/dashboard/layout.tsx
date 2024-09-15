c1d94afbcc065706090ef31ba7b93e8b3b63334d
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-09-14 21:34:45 -0600

    added functions to check user subscription and is logged in

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -5,17 +5,14 @@ import { SecurityModal } from '@/components/reusables/modals/user/security';
 import { UpgradeModal } from '@/components/reusables/modals/user/upgrade';
 import AiUsageModal from '@/components/reusables/modals/user/usage';
 import { WebhookModal } from '@/components/reusables/modals/user/webhook';
-import { Toaster } from '@/components/ui/sonner';
+import { ScrollArea } from '@/components/ui/scroll-area';
 import {
   mockBillingHistory,
   mockPaymentDetails
 } from '@/types/_faker/profile/userData';
 import { mockSubscriptions } from '@/types/_faker/profile/userSubscription';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import type { Metadata } from 'next';
-import { useUserValidation } from '@/lib/utils/profile/managing';
-
+import { Toaster } from '@/components/ui/sonner';
 export const metadata: Metadata = {
   title:
     'Lead Ignite Dashboard | Real Estate Property Search & Market Analysis',
@@ -28,14 +25,6 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-
-  // Mock session and subscription for demonstration purposes (replace with actual data)
-  const subscription = mockSubscriptions[0]; // Replace with real data fetching logic if available
-
-  // Call the user validation hook (this will handle the redirects)
-  useUserValidation(subscription); // Hook will redirect to /logout or /profile based on login/subscription status
-
   return (
     <div className="flex">
       <Sidebar />
@@ -43,12 +32,11 @@ export default function DashboardLayout({
         <Header />
         {children}
       </main>
-      {/* Modals */}
       <AiUsageModal />
       <BillingModal
         billingHistory={mockBillingHistory}
         paymentDetails={mockPaymentDetails}
-        subscription={subscription}
+        subscription={mockSubscriptions[0]}
       />
       <SecurityModal />
       <UpgradeModal />
