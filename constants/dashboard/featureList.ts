426d04d9425bf53cfeb38bd72066420a3409a8d9
Tyrique Daniel <tyrique1.daniel@gmail.com>
2024-09-17 22:01:25 -0600

    added subsctiption modal

---
+++ constants/dashboard/featureList.ts
@@ -0,0 +1,22 @@
+import { PhoneCall, UserCheck, TrendingUp } from 'lucide-react';
+
+export const SubscriptionFeatures = [
+  {
+    title: 'Calling powered by artificial intelligence.',
+    subtitle:
+      'Our AI technology allows for natural phrasing and dynamic engagements using human-like voice models. Advanced calling algorithms ensure optimal results and quality leads.',
+    icon: PhoneCall
+  },
+  {
+    title: 'Human vs OttoLeads.',
+    subtitle:
+      'It takes a human on a dialer 3 years to do what OttoLeads can do in one day. Let us do the heavy lifting while you focus on closing deals.',
+    icon: UserCheck
+  },
+  {
+    title: 'Scale rapidly and dominate your market.',
+    subtitle:
+      'Scaling your business used to take years; with OttoLeads, you can scale in months.',
+    icon: TrendingUp
+  }
+];
